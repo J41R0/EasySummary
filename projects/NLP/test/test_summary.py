4ee599816dc3bf9516ff5f5043b44f7735fea8d3
J41R0 <jairo.lefebre@gmail.com>
2020-01-28 10:06:34 -0500

    Add summarization test

--- projects/NLP/test/test_summary.py
+++ projects/NLP/test/test_summary.py
@@ -1,8 +1,31 @@
 import unittest
+import json
 from projects.NLP.summary import get_summary
 
 
 class SummaryTest(unittest.TestCase):
 
     def test_summarization_method(self):
-        pass
+        op_str = """{
+          "opinions": [
+            {
+              "id": 1,
+              "text": "The cats are really cute animals"
+            },
+             {
+              "id": 2,
+              "text": "The cutest pets are cats. They are my favorite animal."
+            },
+             {
+              "id": 3,
+              "text": "I am a dog lover and hate cats"
+            }
+        ]}
+        """
+        op_dict = json.loads(op_str)
+        summary = get_summary(1, op_dict)
+        self.assertEqual("The cats are really cute animals", summary)
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
